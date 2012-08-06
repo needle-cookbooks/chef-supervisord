0f6b7c1e58a2f8a200d5c9deb80438a2d731003c
Cameron Johnston <cameron@needle.com>
2012-08-05 22:11:47 -0600

    making the allowed classes for autorestart more permissive

--- resources/program.rb
+++ resources/program.rb
@@ -4,5 +4,5 @@ attribute :name, :kind_of => String, :name_attribute => true
 attribute :command, :kind_of => String, :required => true
 attribute :directory, :kind_of => String
 attribute :autostart, :kind_of => [TrueClass,FalseClass], :default => true
-attribute :autorestart, :kind_of => String, :default => "unexpected"
+attribute :autorestart, :kind_of => [String,TrueClass,FalseClass], :default => "unexpected"
 attribute :user, :kind_of => String
